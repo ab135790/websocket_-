91252c87eef9234e7e110191a067c453082e0dc7
ab135790 <275284429@qq.com>
2020-05-29 17:31:09 +0800

    2020-5-29

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-05-27 13:56:55
- * @LastEditTime: 2020-05-28 17:07:12
+ * @LastEditTime: 2020-05-29 16:52:36
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \demo\server\index.js
@@ -12,21 +12,23 @@ const wss = new WebSocket.Server({ noServer: true });
 const server = http.createServer();
 const jwt = require('jsonwebtoken');
 
+const timeInterval = 1000;
 // 房间分组
-
 let group = {};
 
 // 多聊天室的功能
 // roomid => 对应相同的roomid进行广播消息
 
 wss.on('connection', function connection (ws) {
+    // 初始的心跳连接状态
+    ws.isAlive = true;
     console.log('one client is connected');
     // 接收客户端的消息
     ws.on('message', function(msg) {
         const msgObj = JSON.parse(msg);
         if (msgObj.event === 'enter') {
             ws.name = msgObj.message;
-            ws.roomid = msgObj.roomid
+            ws.roomid = msgObj.roomid;
             if (typeof group[ws.roomid] === 'undefined') {
                 group[ws.roomid] = 1;
             } else {
@@ -38,6 +40,10 @@ wss.on('connection', function connection (ws) {
             jwt.verify(msgObj.message, 'secret', (err, decode) => {
                 if (err) {
                     // websocket返回前台鉴权失败信息
+                    ws.send(JSON.stringify({
+                        event: 'noauth',
+                        message: 'please auth again'
+                    }))
                     return;
                 } else {
                     // 鉴权通过
@@ -46,14 +52,16 @@ wss.on('connection', function connection (ws) {
                     return;
                 }
             })
+            return
         }
         // 拦截非鉴权的请求
         if (!ws.isAuth) {
-            ws.send(JSON.stringify({
-                event: 'noauth',
-                message: 'please auth again'
-            }))
             return;
+        }
+        // 心跳檢測
+        if (msgObj.event === 'heartbeat' && msgObj.message === 'pong') {
+            ws.isAlive = true;
+            return
         }
         // 主动发送消息给客户端
         // ws.send('server:' +  msg)
@@ -104,3 +112,22 @@ server.on('upgrade', function upgrade(request, socket, head) {
   });
    
   server.listen(3000);
+
+  setInterval(() => {
+      wss.clients.forEach(ws => {
+          if (!ws.isAlive && ws.roomid) {
+            console.log('in');
+            group[ws.roomid]--;
+            delete ws['roomid'];
+            return ws.terminate(); // 终止或关闭连接
+          }
+        //   主动发送心跳检测请求
+        // 当客户端返回了消息之后，主动设置了flag为在线
+        ws.isAlive = false; // 周期一开始为false
+        ws.send(JSON.stringify({
+            event: 'heartbeat',
+            message: 'ping',
+            num: group[ws.roomid]
+        }))
+      })
+  }, timeInterval)
